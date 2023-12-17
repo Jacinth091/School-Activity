5059f0d364b3776ff50a9198831b6d9d09e8bac9
Jacinth <jacinthcedricbarral@gmail.com>
2023-12-17 08:41:36 +0800

    Little Improvements (on the design and ui elements)

--- js/Javascript-Activity(WEBDEV)/integer-calculator.js
+++ js/Javascript-Activity(WEBDEV)/integer-calculator.js
@@ -10,37 +10,60 @@ function Calculate() {
     modulo = num1 % num2;
     average = (num1 + num2) / 2;
     result = `Sum: ${sum} <br> Product: ${product} <br> Quotient: ${parseFloat(quotient.toFixed(2))} <br> Modulo: ${parseFloat(modulo.toFixed(2))} <br> Average: ${parseFloat(average.toFixed(2))} `;
+    document.getElementById('allop').innerHTML = "All Operation: ";
+    document.getElementById('res-cont').style.display = "block";
+
 
     
     if (isNaN(num1) || isNaN(num2)) {
-        document.getElementById('result').innerHTML = "Please Input Any Value!";
+        alert("Please Input Any Value!");
+        document.getElementById('p-res').style.display = "none";
+        document.getElementById('allop').style.display = "none";
+        document.getElementById('res-cont').style.display = "none";
+
     }
     else {
         try {
             switch (operator) {
             case "+":
+                document.getElementById('p-res').style.display = "block";
+                document.getElementById('allop').style.display = "none";
                 document.getElementById('result').innerHTML = `Sum: ${sum}`;
             break;
             case "-":
+                document.getElementById('p-res').style.display = "block";
+                document.getElementById('allop').style.display = "none";
                 document.getElementById('result').innerHTML = `Difference: ${differ}`;
             break;
             case "*":
+                document.getElementById('p-res').style.display = "block";
+                document.getElementById('allop').style.display = "none";
                 document.getElementById('result').innerHTML = `Product: ${product}`;
             break;
             case "/":
+                document.getElementById('p-res').style.display = "block";
+                document.getElementById('allop').style.display = "none";
                 document.getElementById('result').innerHTML = `Quotient: ${parseFloat(quotient.toFixed(2))}`;
             break;
             case "%":
+                document.getElementById('p-res').style.display = "block";
+                document.getElementById('allop').style.display = "none";
                 document.getElementById('result').innerHTML = `Modulo: ${parseFloat(modulo.toFixed(2))}`;
                 break;
             case "Ave": //Average
+                document.getElementById('p-res').style.display = "block";
+                document.getElementById('allop').style.display = "none";
                 document.getElementById('result').innerHTML = `Average: ${average}`;
             break;
             case "All": // All Operation
                 document.getElementById('result').style.height = "auto";
-                document.getElementById('result').innerHTML = `All Operation: <br> ${result}`;
+                document.getElementById('p-res').style.display = "none";
+                document.getElementById('allop').style.display = "block";
+                document.getElementById('result').innerHTML = `${result}`;
             break;
             default:
+                document.getElementById('p-res').style.display = "block";
+                document.getElementById('allop').style.display = "none";
                 document.getElementById('result').innerHTML = 'Error!';
             break;
             }
